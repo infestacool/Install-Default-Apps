312b682110f7fd4f1918ae999e8551c8d9e552c8
infestacool <infestacool@gmail.com>
2016-07-28 16:22:12 -0400

    Update Install_Default_Apps.sh

--- Install_Default_Apps.sh
+++ Install_Default_Apps.sh
@@ -16,7 +16,7 @@ install_app ()	{
 curl https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg > $temp/Chrome.dmg
 	install_app Chrome.dmg
 	
-curl http://mirror.lstn.net/videolan/vlc/2.2.4/macosx/vlc-2.2.4.dmg > $temp\VLC.dmg
+curl http://mirror.lstn.net/videolan/vlc/2.2.4/macosx/vlc-2.2.4.dmg > $temp/VLC.dmg
 	install_app VLC.dmg
 
 wget -O $temp/Firefox.dmg "http://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
