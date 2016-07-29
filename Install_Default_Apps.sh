07cca8f7a27ed422380ae9a91554dfca2d1a8c1a
infestacool <chris@moran.ws>
2016-07-29 15:57:05 -0400

    Switching to Functions for install

--- Install_Default_Apps.sh
+++ Install_Default_Apps.sh
@@ -4,14 +4,28 @@ temp=$TMPDIR$(uuidgen)
 mkdir -p $temp/mount
 
 # wget
-curl -s https://raw.githubusercontent.com/rudix-mac/rpm/2015.10.20/rudix.py | sudo python - install rudix
-sudo rudix install wget
+	curl -s https://raw.githubusercontent.com/rudix-mac/rpm/2015.10.20/rudix.py | sudo python - install rudix
+	sudo rudix install wget
 
+# Define Install Functions
 	install_app ()	{
-	echo Installing $1
-	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/$1
-	cp -r $temp/mount/*.app /Applications
-	hdiutil detach $temp/mount
+		echo Installing $1
+		yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/$1
+		cp -r $temp/mount/*.app /Applications
+		hdiutil detach $temp/mount
+		}
+	install_dmg_pkg () {
+		wget --tries=0 --read-timeout=20 --no-check-certificate -O $temp/$1 $2
+		yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/$1
+		sudo installer -pkg $temp/mount/$3 -target /
+		hdiutil detach $temp/mount
+		}
+	
+	install_dmg_pkg_curl () {
+		curl $2 > $1
+		yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/$1
+		sudo installer -pkg $temp/mount/$3 -target /
+		hdiutil detach $temp/mount
 		}
 
 # Chrome
@@ -19,7 +33,7 @@ sudo rudix install wget
 	install_app Chrome.dmg
 
 # VLC
-	wget -O  $temp/VLC.dmg https://get.videolan.org/vlc/2.2.4/macosx/VLC-webplugin-2.2.4.dmg
+	wget --no-check-certificate -O  $temp/VLC.dmg https://get.videolan.org/vlc/2.2.4/macosx/VLC-webplugin-2.2.4.dmg
 	install_app VLC.dmg
 
 # Firefox	
@@ -30,37 +44,24 @@ sudo rudix install wget
 	wget --no-check-certificate -O $temp/MBAM.dmg https://store.malwarebytes.com/342/purl-mbamm-dl
 	install_app MBAM.dmg
 
-#Silverlight
-	wget -O $temp/Silverlight.dmg "http://go.microsoft.com/fwlink/?LinkID=229322"
-	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/Silverlight.dmg
-	sudo installer -pkg $temp/mount/silverlight.pkg -target /
-	hdiutil detach $temp/mount
-
-# Link from http://macadmins.software/
+# Office 2016 for O365 Activation. Link from http://macadmins.software/
 	wget --tries=0 --read-timeout=20 --no-check-certificate -O $temp/Office.pkg http://go.microsoft.com/fwlink/?linkid=525133
 	sudo installer -pkg $temp/Office.pkg -target /
 
-# XQuartz 
-	wget --no-check-certificate -O $temp/XQuartz.dmg https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.9.dmg
-	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/XQuartz.dmg
-	sudo installer -pkg $temp/mount/XQuartz.pkg -target /
-	hdiutil detach $temp/mount
+#Silverlight
+	install_dmg_pkg Silverlight.dmg "http://go.microsoft.com/fwlink/?LinkID=229322" silverlight.pkg
 
-# Flash 
-	curl https://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_22_osx_pkg.dmg > $temp/Flash.dmg
-	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/Flash.dmg
-	sudo installer -pkg $temp/mount/Install\ Adobe\ Flash\ Player.pkg -target /
-	hdiutil detach $temp/mount
+# XQuartz 
+	install_dmg_pkg XQuartz.dmg "https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.9.dmg" XQuartz.pkg 
 
 # HP Printer Drivers
-	wget --no-check-certificate -O $temp/HPDrivers.dmg http://support.apple.com/downloads/DL907/en_US/hpprinterdriver3.1.dmg
-	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/HPDrivers.dmg
-	sudo installer -pkg $temp/mount/HewlettPackardPrinterDrivers.pkg -target /
-	hdiutil detach $temp/mount
+	install_dmg_pkg HPDrivers.dmg "http://support.apple.com/downloads/DL907/en_US/hpprinterdriver3.1.dmg" HewlettPackardPrinterDrivers.pkg
 
+# Ricoh Printer Drivers 
+	install_dmg_pkg RicohDrivers.dmg "http://support.apple.com/downloads/DL1867/en_US/ricohprinterdrivers3.0.dmg" RicohPrinterDrivers.pkg
 
-# Ricoh Printer Drivers http://support.apple.com/downloads/DL1867/en_US/ricohprinterdrivers3.0.dmg
-
+# Flash 
+	install_dmg_pkg_curl Flash.dmg "https://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_22_osx_pkg.dmg" "Install Adobe Flash Player.pkg"
 rm -r $temp
 
 #diskutil verifyVolume /
