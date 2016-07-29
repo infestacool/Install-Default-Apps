d39c68a1c45940d39888df8aadf8ef4f92083ad0
infestacool <chris@moran.ws>
2016-07-29 16:28:51 -0400

    test path

--- Install_Flash.sh
+++ Install_Flash.sh
@@ -7,12 +7,12 @@ install_dmg_pkg_curl () {
 		echo $1
 		curl -m 60 $2 > $1
 		yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/$1
-		sudo installer -pkg $temp/mount/$3 -target /
+		sudo installer -pkg $temp/mount/Install\ Adobe\ Flash\ Player.pkg -target /
 		hdiutil detach $temp/mount
 		}
 
 # Flash 
-	install_dmg_pkg_curl Flash.dmg "https://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_22_osx_pkg.dmg" Install\ Adobe\ Flash\ Player.pkg
+	install_dmg_pkg_curl Flash.dmg "https://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_22_osx_pkg.dmg"
 
 
 exit 0
