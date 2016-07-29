51f021b0b767bf4c9fe26d503bd537f3826e127c
infestacool <chris@moran.ws>
2016-07-29 15:28:45 -0400

    fix mount add HP Drivers
    Fixed mounting o dmg files
    added HP drivers

--- Install_Default_Apps.sh
+++ Install_Default_Apps.sh
@@ -3,51 +3,62 @@
 temp=$TMPDIR$(uuidgen)
 mkdir -p $temp/mount
 
+# wget
 curl -s https://raw.githubusercontent.com/rudix-mac/rpm/2015.10.20/rudix.py | sudo python - install rudix
 sudo rudix install wget
 
-install_app ()	{
+	install_app ()	{
 	echo Installing $1
 	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/$1
 	cp -r $temp/mount/*.app /Applications
 	hdiutil detach $temp/mount
-	}
+		}
 
-curl -m 30 https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg > $temp/Chrome.dmg
+# Chrome
+	curl -m 30 https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg > $temp/Chrome.dmg
 	install_app Chrome.dmg
 
-wget -O  $temp/VLC.dmg https://get.videolan.org/vlc/2.2.4/macosx/VLC-webplugin-2.2.4.dmg
+# VLC
+	wget -O  $temp/VLC.dmg https://get.videolan.org/vlc/2.2.4/macosx/VLC-webplugin-2.2.4.dmg
 	install_app VLC.dmg
-	
-wget -O $temp/Firefox.dmg "http://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
+
+# Firefox	
+	wget -O $temp/Firefox.dmg "http://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
 	install_app Firefox.dmg
 
-wget --no-check-certificate -O $temp/MBAM.dmg https://store.malwarebytes.com/342/purl-mbamm-dl
+# Malware Bytes
+	wget --no-check-certificate -O $temp/MBAM.dmg https://store.malwarebytes.com/342/purl-mbamm-dl
 	install_app MBAM.dmg
 
-wget -O $temp/Silverlight.dmg "http://go.microsoft.com/fwlink/?LinkID=229322"
-	hdiutil attach $temp/Silverlight.dmg
-	sudo installer -pkg /Volumes/Silverlight/silverlight.pkg -target /
-	hdiutil detach /Volumes/Silverlight
+#Silverlight
+	wget -O $temp/Silverlight.dmg "http://go.microsoft.com/fwlink/?LinkID=229322"
+	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/Silverlight.dmg
+	sudo installer -pkg $temp/mount/silverlight.pkg -target /
+	hdiutil detach $temp/mount
 
 # Link from http://macadmins.software/
-wget --tries=0 --read-timeout=20 --no-check-certificate -O $temp/Office.pkg http://go.microsoft.com/fwlink/?linkid=525133
+	wget --tries=0 --read-timeout=20 --no-check-certificate -O $temp/Office.pkg http://go.microsoft.com/fwlink/?linkid=525133
 	sudo installer -pkg $temp/Office.pkg -target /
 
 # XQuartz 
-wget --no-check-certificate -O $temp/XQuartz.dmg https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.9.dmg
-	hdiutil attach $temp/XQuartz.dmg
-	sudo installer -pkg /Volumes/XQuartz-2.7.9/XQuartz.pkg -target /
-	hdiutil detach /Volumes/XQuartz-2.7.9
+	wget --no-check-certificate -O $temp/XQuartz.dmg https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.9.dmg
+	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/XQuartz.dmg
+	sudo installer -pkg $temp/mount/XQuartz.pkg -target /
+	hdiutil detach $temp/mount
 
 # Flash 
-curl https://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_22_osx_pkg.dmg > $temp/Flash.dmg
-	hdiutil attach $temp/Flash.dmg
-	sudo installer -pkg /Volumes/Flash\ Player/Install\ Adobe\ Flash\ Player.pkg -target /
-	hdiutil detach /Volumes/Flash\ Player
+	curl https://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_22_osx_pkg.dmg > $temp/Flash.dmg
+	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/Flash.dmg
+	sudo installer -pkg $temp/mount/Install\ Adobe\ Flash\ Player.pkg -target /
+	hdiutil detach $temp/mount
+
+# HP Printer Drivers
+	wget --no-check-certificate -O $temp/HPDrivers.dmg http://support.apple.com/downloads/DL907/en_US/hpprinterdriver3.1.dmg
+	yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/HPDrivers.dmg
+	sudo installer -pkg $temp/mount/HewlettPackardPrinterDrivers.pkg -target /
+	hdiutil detach $temp/mount
 
 
-# HP Printer drivers http://support.apple.com/downloads/DL907/en_US/hpprinterdriver3.1.dmg
 # Ricoh Printer Drivers http://support.apple.com/downloads/DL1867/en_US/ricohprinterdrivers3.0.dmg
 
 rm -r $temp
