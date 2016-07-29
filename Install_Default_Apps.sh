0b850e1333766aff33a65810c4ba8c12fb0c08b3
infestacool <chris@moran.ws>
2016-07-29 14:40:40 -0400

    Flash
    Install Adobe Flash

--- Install_Default_Apps.sh
+++ Install_Default_Apps.sh
@@ -14,7 +14,7 @@ install_app ()	{
 
 curl https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg > $temp/Chrome.dmg
 	install_app Chrome.dmg
-# wget --tries=0 --read-timeout=20 --no-check-certificate -O Chrome.dmg https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+
 wget -O  $temp/VLC.dmg https://get.videolan.org/vlc/2.2.4/macosx/VLC-webplugin-2.2.4.dmg
 	install_app VLC.dmg
 	
@@ -36,12 +36,17 @@ wget --tries=0 --read-timeout=20 --no-check-certificate -O $temp/Office.pkg http
 # XQuartz 
 wget --no-check-certificate -O $temp/XQuartz.dmg https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.9.dmg
 	hdiutil attach $temp/XQuartz.dmg
-	sudo installer -pkg /Volumes/XQuartz-2.7.9/XQuartz.pkg -target /
-	hdiutil detach /Volumes/XQuartz-2.7.9
+
+
+# Flash 
+curl https://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_22_osx_pkg.dmg > $temp/Flash.dmg
+	hdiutil attach $temp/Flash.dmg
+	sudo installer -pkg /Volumes/Flash\ Player/Install\ Adobe\ Flash\ Player.pkg -target /
+	hdiutil detach /Volumes/Flash\ Player
+
 
 # HP Printer drivers http://support.apple.com/downloads/DL907/en_US/hpprinterdriver3.1.dmg
 # Ricoh Printer Drivers http://support.apple.com/downloads/DL1867/en_US/ricohprinterdrivers3.0.dmg
-# Flash https://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_22_osx_pkg.dmg
 
 rm -r $temp
 
