a72ed0c00392971c361a219c989fb915d231c314
infestacool <infestacool@gmail.com>
2016-08-01 16:33:37 -0400

    Update Install_Default_Apps.sh

--- Install_Default_Apps.sh
+++ Install_Default_Apps.sh
@@ -4,13 +4,14 @@ temp=$TMPDIR$(uuidgen)
 mkdir -p $temp/mount
 
 # wget
+	printf "\033[1;31mInstalling Rudix & wget \033[0m\n"
 	curl -s https://raw.githubusercontent.com/rudix-mac/rpm/2015.10.20/rudix.py | sudo python - install rudix
 	sudo rudix install wget
 
 
 # Function install .app inside DMG Input: Filename.dmg "URL"
 	install_app ()	{
-		printf "\033[1;31mInstalling \033[0m\n"  $1
+		printf "\033[1;31mInstalling $1 \033[0m\n"
 		wget --tries=0 --read-timeout=20 --no-check-certificate -O $temp/$1 $2
 		yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/$1
 		cp -r $temp/mount/*.app /Applications
@@ -19,7 +20,7 @@ mkdir -p $temp/mount
 
 # Function install_app but using curl
 	install_app_curl ()	{
-		echo $1		
+		printf "\033[1;31mInstalling $1 \033[0m\n"
 		curl -m 60 $2 > $temp/$1
 		yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/$1
 		cp -r $temp/mount/*.app /Applications
@@ -28,7 +29,7 @@ mkdir -p $temp/mount
 
 # Function install .pkg inside DMG Input: Filename.dmg "URL" Package.pkg
 	install_dmg_pkg () {
-		echo $1		
+		printf "\033[1;31mInstalling $1 \033[0m\n"
 		wget --tries=0 --read-timeout=20 --no-check-certificate -O $temp/$1 $2
 		yes | hdiutil attach -noverify -nobrowse -mountpoint $temp/mount $temp/$1
 		sudo installer -pkg $temp/mount/$3 -target /
@@ -45,7 +46,7 @@ mkdir -p $temp/mount
 	install_app Firefox.dmg "http://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
 
 # Malware Bytes
-	install_app MBAM.dmg "https://store.malwarebytes.com/342/purl-mbamm-dl"
+	install_app MalwareBytes.dmg "https://store.malwarebytes.com/342/purl-mbamm-dl"
 
 # Silverlight
 	install_dmg_pkg Silverlight.dmg "http://go.microsoft.com/fwlink/?LinkID=229322" silverlight.pkg
@@ -60,12 +61,15 @@ mkdir -p $temp/mount
 	install_dmg_pkg RicohDrivers.dmg "http://support.apple.com/downloads/DL1867/en_US/ricohprinterdrivers3.0.dmg" RicohPrinterDrivers.pkg
 
 # Flash Latest version using rtrouton script
+	printf "\033[1;31mInstalling Adobe Flash \033[0m\n"
 	curl https://raw.githubusercontent.com/rtrouton/rtrouton_scripts/master/rtrouton_scripts/install_latest_adobe_flash_player/install_latest_adobe_flash_player.sh | sudo bash
 	
 # Java 8 Latest version using rtrouton script
+	printf "\033[1;31mInstalling Java 8  \033[0m\n"
 	curl https://raw.githubusercontent.com/rtrouton/rtrouton_scripts/master/rtrouton_scripts/install_latest_oracle_java_8_jdk/install_latest_oracle_java_jdk_8.sh | sudo bash
 
 # Office 2016 for O365 Activation. Link from http://macadmins.software/
+	printf "\033[1;31mInstalling Office 2016 for O365 \033[0m\n"
 	wget --tries=0 --read-timeout=20 --no-check-certificate -O $temp/Office.pkg http://go.microsoft.com/fwlink/?linkid=525133
 	sudo installer -pkg $temp/Office.pkg -target /
 	
