fb7cb9e865b7380f1ba75f8211029b9a9d290465
infestacool <infestacool@gmail.com>
2016-08-03 13:01:39 -0400

    Update installIDF9.sh

--- installIDF9.sh
+++ installIDF9.sh
@@ -6,7 +6,7 @@ cd $temp
 curl http://winlndskappprd/Distribute/IdentityFinder/HBS-IDFinder_9.1_[prod]_MACv2/IdentityFinder.pkg > $temp/IdentityFinder.pkg
 curl http://winlndskappprd/Distribute/IdentityFinder/HBS-IDFinder_9.1_[prod]_MACv2/Info.plist > $temp/Info.plist
 
-sudo installer -pkg IdentityFinder.pkg -target /
+sudo installer -pkg $temp/IdentityFinder.pkg -target /
 rm -r $temp
 
 exit 0
