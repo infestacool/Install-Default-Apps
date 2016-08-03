14ffc4de5cd275e8d75611035f4afeb880848913
infestacool <infestacool@gmail.com>
2016-08-03 13:45:12 -0400

    Update installIDF9.sh

--- installIDF9.sh
+++ installIDF9.sh
@@ -6,11 +6,10 @@ temp=$TMPDIR$(uuidgen)
 	curl -s https://raw.githubusercontent.com/rudix-mac/rpm/2015.10.20/rudix.py | sudo python - install rudix
 	sudo rudix install wget
 	
-cd $temp
-wget $temp/IdentityFinder.pkg http://winlndskappprd/Distribute/IdentityFinder/HBS-IDFinder_9.1_[prod]_MACv2/IdentityFinder.pkg
-wget $temp/Info.plist http://winlndskappprd/Distribute/IdentityFinder/HBS-IDFinder_9.1_[prod]_MACv2/Info.plist
+wget IdentityFinder.pkg http://winlndskappprd/Distribute/IdentityFinder/HBS-IDFinder_9.1_[prod]_MACv2/IdentityFinder.pkg
+wget Info.plist http://winlndskappprd/Distribute/IdentityFinder/HBS-IDFinder_9.1_[prod]_MACv2/Info.plist
 
-sudo installer -pkg $temp/IdentityFinder.pkg -target /
+sudo installer -pkg IdentityFinder.pkg -target /
 rm -r $temp
 
 exit 0
